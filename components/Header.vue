b041e981e4a04a0ffeef9ff477e28aa3593f4906
Guillaume <randomnessdev@gmail.com>
2023-08-31 18:38:48 +0200

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,16 @@
 <template>
     <section class="w-full dark">
-        <div class="max-w-screen-xl px-6 py-8 mx-auto space-y-12 text-center lg:pt-6 lg:pb-32 lg:px-12">
+        <div class="max-w-screen-xl px-6 py-8 mx-auto text-center lg:space-y-16 2xl:space-y-24 lg:pb-44 2xl:pb-64 lg:px-12">
             <div class="flex justify-between w-full">
                 <img src="~/assets/img/logo_rempart_negatif.svg" alt="Logo Rempart Mutuelle" class="w-24 m-0 md:w-32">
                 <img src="~/assets/img/logo_hg_header.svg" alt="Logo HG - Agir avec vous !" class="w-24 m-0 md:w-32">
             </div>
             <div>
             <h1
-                class="mb-4 text-3xl font-semibold text-gray-900 uppercase md:text-2xl lg:text-3xl dark:text-white">
+                class="mb-4 text-3xl font-semibold text-gray-900 uppercase md:text-2xl lg:text-4xl dark:text-white">
                 Prévoyance Rempart Mutuelle
             </h1>
-            <p class="mb-8 text-lg font-semibold text-gray-500 lg:text-xl sm:px-16 xl:px-32 dark:text-white">
+            <p class="mb-8 text-lg font-semibold text-gray-500 lg:text-2xl sm:px-16 xl:px-32 dark:text-white">
                 Découvrez
                 l’offre exclusive réservée aux agents du Conseil Départemental<br>
                 de la Haute-Garonne.</p>
